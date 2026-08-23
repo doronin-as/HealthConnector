e846a95053582856a5e73b765a3d74544078cb57
Alexander <53084491+doronin-as@users.noreply.github.com>
2026-08-23 12:51:36 +0300

    Bump HealthConnector to 1.6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "ru.doronin.healthconnector"
         minSdk = 26
         targetSdk = 35
-        versionCode = 9
-        versionName = "1.5.3"
+        versionCode = 10
+        versionName = "1.6.0"
     }
 
     buildTypes {
