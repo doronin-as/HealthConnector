9fa02d381bdcb84974a2c514f187458ffe50c792
Alexander <53084491+doronin-as@users.noreply.github.com>
2026-09-02 01:46:55 +0300

    Bump version to 1.6.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "ru.doronin.healthconnector.stable"
         minSdk = 26
         targetSdk = 35
-        versionCode = 17
-        versionName = "1.6.3"
+        versionCode = 18
+        versionName = "1.6.4"
     }
 
     buildTypes {
