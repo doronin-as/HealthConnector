8f015bc0a9075c1d701f4d2b039d4a5d95a317dd
Alexander <53084491+doronin-as@users.noreply.github.com>
2026-09-13 12:03:29 +0300

    Bump version to 1.6.17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "ru.doronin.healthconnector.stable"
         minSdk = 26
         targetSdk = 35
-        versionCode = 30
-        versionName = "1.6.16"
+        versionCode = 31
+        versionName = "1.6.17"
         manifestPlaceholders["appLabel"] = "Health Connector"
     }
 
